91f352fb8b597b63ef34b6123a01c9bbb3004e05
K I N G <nelsonmichael4real@gmail.com>
2021-01-30 06:57:48 +0800

    added restart functionality

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ let timer = document.getElementById("timer");
 let wordToType = document.getElementById("word-to-type")
 let userInput = document.getElementById("user-input");
 let waiting = document.getElementById("wait-time");
+let toggleRestart = document.getElementById("game-restart");
 
 // global variables
 const words = ["abyssal","abysses","abyssopelagic", "acacia","acacias","academe","academes", "accompanyist","accompanyists","accomplice","accomplices","accomplish","accomplishable","accomplished","bulbing","bulblet","bulblets","bulbosities","bulbosity","bulbous","bulbously","bulbousness","bulbousnesses","bulge","bulged","bulger","cartages","cartas","carte","carted","cartel","cartelisation","cartelisations","cartelise","cartelised","cartelises","cartelising","cartelism","cartelisms","cartelist","cartelists","cartelization","cartelizations","offstage","offstages","offtake","offtakes","offtrack","offynyanza","nyanzas","nyas","nyases","nybble","nybbles","nychthemeral","nychthemeron","nychthemerons","nyctaginaceous","nyctalopesjoggling","jogpants","jogs","jogtrot","jogtrots","johannes","johanneses","john","johnboat","johnboats","johnnie","johnnies","johnny","johnnycake","johns","iatrical","iatrochemical","iatrochemist","iatrochemistry","hysterically","hystericky","hysterics","hysteritis","hysteritises","hysterogenic","hysterogenies","sepultured","sepultures","sepulturing","sequacious","sequaciously","sequaciousness","sequacities","sequacity","sequel","sequela","sequelae","sequelise","sequelised","sequelises","tostada","tosticated","tostication","tostications","tot","totable","total","totaled","totaling","totalisation","wholegrain","wholehearted","wholeheartedly","wholemeal","wholemeals","wholeness","wholenesses","worrying","wholesale","wholesome","wholesomely","water","zymolyses","zymolysis","zymolytic","zymome","zymomes","zymometer","zymometers","zymosan","zymosans","zymoses","zymosimeter","zymosimeters","zymosis"];
@@ -15,7 +16,7 @@ let score = 0;
 const initializeGame= ()=>{
     updateWordToType();
 
-    userInput.addEventListener('input', matchWords)
+    userInput.addEventListener('input', matchWords);
 
     setInterval(updateTimer, 1000);
 }
@@ -36,13 +37,13 @@ const updateWordToType = ()=>{
 }
 
 
-// this function creates the game timer and disables user input when time runs out
+// this function creates the game timer, disables user input when time runs out, and also toggles restart visibilty
 const updateTimer = ()=>{
   if(time>0){
      time--;
   }else{
       userInput.disabled = true;
-      console.log(score);
+      toggleRestart.style.display = "block";
   }
   timer.textContent = `${time}s`;
 }
@@ -62,3 +63,4 @@ const waitTimeCountDown = ()=>{
 
 setInterval(waitTimeCountDown, 1000);
 setTimeout(initializeGame, 5200);
+
